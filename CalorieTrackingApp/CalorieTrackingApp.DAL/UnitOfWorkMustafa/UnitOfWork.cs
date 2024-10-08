cbd0d7a3b57684610ebce3d427721eba6af8e1cf
mask06 <Mustafaygar3903@gmail.com>
2024-10-08 17:56:44 +0300

    UnitOfWorkMustafa Eklendi

---
+++ CalorieTrackingApp/CalorieTrackingApp.DAL/UnitOfWorkMustafa/UnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalorieTrackingApp.DAL.UnitOfWorkMustafa
+{
+    internal class UnitOfWork
+    {
+    }
+}
